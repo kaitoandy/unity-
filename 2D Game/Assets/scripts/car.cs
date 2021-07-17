67b632bb5c195f4dcf517a9f341c7f71d293e69d
kaitoandy <86511386+kaitoandy@users.noreply.github.com>
2021-07-17 19:40:07 +0800

    程式基礎練習

--- 2D Game/Assets/scripts/car.cs
+++ 2D Game/Assets/scripts/car.cs
@@ -1,18 +1,99 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine; //引用Unity引擎提供的API (Unity Engine 命名空間)
 
-public class car : MonoBehaviour
+//類別
+//語法 類別關鍵字 腳本名稱
+public class Car : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   #region 註解
+    //單行註解:添加說明 翻譯 紀錄等等...會被程式忽略
+    //kaito 2021.07.17 (六)開發汽車腳本
+    /*多行註解
+     *多行註解
+     *多行註解
+     *多行註解
+     */
+     #endregion
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   #region 認識欄位與四大常用類型
+     //欄位屬性:輔助欄位添加功能
+     //語法:[屬性名稱(屬性值)]
+     //欄位:儲存簡單的資料
+     //語法:
+     //修飾詞 資料類型 欄位名稱 指定符號 預設值 結尾
+     //指定符號=
+     //修飾詞
+     //1.私人 private 預設-不顯示
+     //2.公開 public 預設-顯示
+
+     //Unity 常用四大類型
+     //整數 int 例:1,99,0,-123
+     //浮點數: float 例:2.3,3.1415,-1.123
+     //字串: string 例:BMW,賓士,對話內容
+     //布林值 bool 例:true,false
+
+     //定義欄位
+     //Unity以屬性 Inspector 面板上的值為主
+     public float weight = 3.5f;
+     public int cc = 2000;
+     public string brand = "賓士";
+     public bool windowSky = true;
+     //標題:[Header(字串)]
+
+     [Header("輪胎數量")]
+     public int wheelCount = 4;
+     //提示:[Tooltip(字串)]
+
+     [Tooltip("這個欄位作用是設定汽車的高度")]
+     public float height = 1.5f;
+
+     //範圍 :[Range(最大值 最小值)]-僅限數值類型 float或 int
+     [Range(2,10)]
+     public int doorCount;
+     #endregion 
+     
+  #region 其他類型
+     //顏色 Color
+     public Color Color1;             // 不指定為黑
+     public Color red = Color.red;    //使用預設值
+     public Color yello = Color.yello;
+     public Color ColorCustom1 = new Color(0.5f,0.5f,0); //自訂顏色(RGB)
+     public Color ColorCustom2 = new Color(0.5f,0.,0.5f,0); //自訂顏色(RGB)
+
+     //座標 2-4 維 Vector2-4
+     //保持數值資訊 浮點數
+     public Vector2 v2;
+     public Vector2 v2Zero = Vector2.zero;
+     public Vector2 v2one = Vector2.one;
+     public Vector2 v2Up = Vector2.up;
+     public Vector2 v2Right = Vector2.right;
+     public Vector2 v2Costum = new Vector2(-99.5f,100.5f)
+
+
+
+     //按鍵類型
+     public Keycode kc;
+     public Keycode forwar = Keycode.D;
+     public Keycode attack = Keycode.Mouse0; //左鍵0 右鍵1 滾輪2
+
+     //遊戲物件與元件
+     public GameObject goCamera; //遊戲物件包含場景上的以及專案內的預製物
+     //元件僅限於存放屬性面板有此元件的物件
+
+     public Transfrom tracCar;
+     public SpriteRenderer sprPicture;
+
+
+     #endregion 
+     
+    #region 事件
+     //開始事件:撥放遊戲時執行一次,處理初始化
+     private void Start ()
+     {
+        //輸出(任何類型資料);
+        print("你好");
+     }
+
+   
+
+     #endregion 
 }
---
+++ 2D Game/Assets/scripts/player.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+#region 欄位
+[Header("移動速度"),Range(0,1000)]
+public float speed = 10.5;
+[Header("跳躍高度"),Range(0,3000)]
+public int hight = 100;
+[Header("血量"), Range(0,100)]
+public float HP = 100;
+[Header("血量"),Tooltip(用來儲存腳色是否在地板上的資訊,在地板上 true,不在地板上 false)]
+public bool isGround;
+
+private Audiosource aud;
+private Rigidbody2D rig;
+private Animator ani;
+
+
+
+
+#endregion
+}
