9432ebc833ffae5bbe3e2c124ba7cd31910c6217
kaitoandy <86511386+kaitoandy@users.noreply.github.com>
2021-08-07 21:01:48 +0800

    主角設定及 怪物設定

---
+++ 2D Game/Assets/scripts/CameraComtrol.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+/// <summary>
+/// 攝影機追蹤目標
+/// </summary>
+public class CameraComtrol : MonoBehaviour
+{
+    #region 欄位
+    [Header("追蹤速度"), Range(0, 100)]
+    public float speed = 10;
+    [Header("要追蹤物件名稱")]
+    public string nameTreget;
+    [Header("左右限制")]
+    public Vector2 limitHorizontal;
+
+    /// <summary>
+    /// 追蹤目標
+    /// </summary>
+    private Transform target;
+
+
+    #endregion
+
+    #region 事件
+    private void Start()
+    {
+        //很吃效能,所以在start執行
+        //目標變形元件 = 遊戲物件.尋找(物件名稱).變形元件
+        target = GameObject.Find(nameTreget).transform;
+
+    }
+    #endregion
+
+    //較慢更新:在 Update 執行 , 建議用來處理攝影機
+    private void LateUpdate()
+    {
+        Track();
+    }
+
+    #region 方法
+    /// <summary>
+    /// 追蹤目標
+    /// </summary>
+    private void Track()
+    {
+        Vector3 posCamera = transform.position;     //A點:攝影機座標
+        Vector3 posTarget = target.position;        //B點:目標物座標
+
+
+        //運算後的結果座標 = 取得A點攝影機 與 B點目標物 之間的座標
+        Vector3 posReault = Vector3.Lerp(posCamera, posTarget, speed * Time.deltaTime);
+
+        //攝影機Z軸放回預設 -10 避免看不到2D物件
+        posReault.z = -10;
+
+        posReault.x = Mathf.Clamp(posReault.x, limitHorizontal.x, limitHorizontal.y);
+
+        //此物件座標 指定為 運算後的結果座標
+        transform.position = posReault;
+    }
+    #endregion
+
+
+
+
+}
---
+++ 2D Game/Assets/scripts/LeamLerp.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class LeamLerp : MonoBehaviour
+{
+    public float a = 0, b = 10f;
+    public float result;
+
+    public float c = 0, d = 100;
+
+    public Vector2 v2A = Vector2.zero;
+    public Vector2 v2B = Vector2.one * 100;
+
+
+    private void Start()
+    {
+        //認識插值 Lerp :取得兩點間的值
+        //結果 = 數學.插值(A點 , B點, 百分比 0 - 1)
+        result = Mathf.Lerp(a, b, 0.5f);
+
+        
+
+    }
+
+    private void Update()
+    {
+        c = Mathf.Lerp(c, d, 0.5f * Time.deltaTime);
+        v2A = Vector2.Lerp(v2A, v2B, 0.8f * Time.deltaTime);
+
+
+    }
+}
