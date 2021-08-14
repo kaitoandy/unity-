57cc0fb9bfdc77ba690645a9ed3032ac285bc879
kaitoandy <86511386+kaitoandy@users.noreply.github.com>
2021-08-15 01:00:37 +0800

    修復選單場景程式

--- 2D Game/Assets/scripts/SceneController.cs
+++ 2D Game/Assets/scripts/SceneController.cs
@@ -6,22 +6,43 @@ public class SceneController : MonoBehaviour
     //Unity 按鈕如何跟腳本溝通
     //1.公開的方法
     //2.需要實體物件掛此腳本
+    //3.按鈕 On Click 設定點擊事件為此物件及要呼叫的方法
 
     /// <summary>
     /// 載入遊戲場景
     /// </summary>
     public void LoadGameScene()
     {
-        SceneManager.LoadScene("遊戲場景");
+        //等待2秒再載入場景
+        //延遲呼叫(方法名稱.延遲時間)
+        //作用: 等待指定時間後再呼叫指定方法
+        Invoke("DelayLoadGameScene", 2);
+        
     }
 
+    //等待一段時間,再執行方法
+    //Invoke 延遲呼叫
+    /// <summary>
+    /// 延遲載入場景
+    /// </summary>
+    private void DelayLoadGameScene()
+    {
+        SceneManager.LoadScene("遊戲場景");
+    }
     /// <summary>
     /// 離開遊戲場景
     /// </summary>
     public void QuitGame()
     {
+        Invoke("DelayQuitGame", 2);
+    }
+
+    /// <summary>
+    /// 延遲離開遊戲
+    /// </summary>
+    private void DelayQuitGame()
+    {
         Application.Quit();   //應用程式.離開() - 離開遊戲
         print("離開遊戲");     //Quit 在編輯器內不會執行
     }
-
 }
