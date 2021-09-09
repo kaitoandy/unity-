472f601871b454b43b7551ebc1979bf12668ea3a
kaitoandy <86511386+kaitoandy@users.noreply.github.com>
2021-09-09 18:15:24 +0800

    補完上課進度

---
+++ 3D game/Assets/Scripts/DialogueData.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+/// <summary>
+/// 儲存對話資料
+/// 1. 對話者名稱
+/// 2. 對話內容 - 多段
+/// </summary>
+/// 
+
+//繼承類別改為 ScriptableObject 腳本化物件
+//代表此類別的內容可以建立物件並儲存於專案內
+//需搭配 CreateAssetMenu 建立素材選項來建立此物件
+[CreateAssetMenu(menuName ="kaitoandy/對話資料" , fileName = "對話資料")]
+public class DialogueData : ScriptableObject
+{
+    [Header("對話者名稱")]
+    public string dialogueTalkerName;
+    [Header("對話內容") ,TextArea(2,5)]
+    public string[] dialogueContents;
+
+
+}
---
+++ 3D game/Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+/// <summary>
+/// 對話系統
+/// 1. 決定說話者名稱
+/// 2. 決定對話內容 - 可以有多段
+/// 3. 顯示每個段落對話完成的動態效果
+/// </summary>
+public class DialogueSystem : MonoBehaviour
+{
+    [Header("對話資料")]
+    public DialogueData date;
+    [Header("對話間隔") , Range(0, 3)]
+    public float invertal = 0.2f;
+    [Header("對話完成圖示")]
+    public GameObject goFinishIcon;
+    [Header("文字介面: 說話者名稱,對話文字內容")]
+    public Text textTalker;
+    public Text textContent;
+    /// <summary>
+    /// 對話系統畫布: 群組元件
+    /// </summary>
+    private CanvasGroup groupDialogue;
+
+    [Header("繼續對話的按鍵")]
+    public KeyCode kcContinute = KeyCode.Mouse0;
+
+    [Header("打字音效")]
+    public AudioClip soundType;
+
+    private AudioSource aud;
+
+    [Header("打字音量"), Range(0, 2)]
+    public float volume = 1; 
+
+
+    private void Start()
+    {
+        aud = GetComponent<AudioSource>();
+        groupDialogue = transform.GetChild(0).GetComponent<CanvasGroup>();
+        StartDialogue();
+    }
+
+    /// <summary>
+    /// 開始對話
+    /// </summary>
+    private void StartDialogue()
+    {
+        StartCoroutine(ShowEveryDialogue());
+    }
+
+    /// <summary>
+    /// 顯示每段對話:並在段落之間等待玩家按繼續按鍵
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator ShowEveryDialogue()
+    {
+        groupDialogue.alpha = 1;                                      //顯示對話畫布 - 透明度為1
+        textTalker.text = date.dialogueTalkerName;                    //顯示對話者名稱
+        textContent.text = "";                                        //對話內容清空
+
+        
+        for (int i = 0; i < date.dialogueContents.Length; i++)         //迴圈,執行每個段落
+        {
+           
+            for (int j = 0; j < date.dialogueContents[i].Length; j++)  //迴圈,執行每個段落內的每一個字
+            {
+                
+                textContent.text += date.dialogueContents[i][j];       //更新對話內容
+                aud.PlayOneShot(soundType, volume);                   //播放音效
+                yield return new WaitForSeconds(invertal);            //打字間隔
+            } 
+             
+            goFinishIcon.SetActive(true);                             //每段話完成後顯示完成圖示
+
+            
+            while (!Input.GetKeyDown(kcContinute))                    //等待玩家按繼續按鈕 - 使用null 為每幀的時間
+            {
+                yield return null;
+            }
+            textContent.text = "";                                    //玩家按下空白鍵後清空內容
+            goFinishIcon.SetActive(false);                            //關閉完成圖示
+
+            if (i == date.dialogueContents.Length - 1) groupDialogue.alpha = 0;
+        }
+    }
+}
